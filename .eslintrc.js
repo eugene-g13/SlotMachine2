b7ca8f11a4768aa0c496b8c97b4b64f848e23f97
Eugene <duzuru@gmail.com>
2021-03-13 15:59:28 +0300

    some prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,28 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    //"plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "import",
-    "plugin:promise/recommended"
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'promise'
-  ],
-  rules: {
-    // "@typescript-eslint/no-unused-vars": "error",
-    // "no-unused-vars": "off"
-  }
-}
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        //"plugin:@typescript-eslint/eslint-recommended",
+        'plugin:@typescript-eslint/recommended',
+        'import',
+        'plugin:promise/recommended',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'promise'],
+    rules: {
+        // "@typescript-eslint/no-unused-vars": "error",
+        // "no-unused-vars": "off"
+        //'no-const-assign': 'warn',
+    },
+};
